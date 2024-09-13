de69f978c03329fa0b0ad6c0c917061328579c72
Sayantann018 <sayantansarkar018@gmail.com>
2024-09-13 08:22:12 +0530

    Add files via upload

---
+++ p.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tictactoe;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author ASUS
+ */
+class p {
+
+    static void add(JLabel title2) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ title2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tictactoe;
+
+/**
+ *
+ * @author ASUS
+ */
+class title2 {
+
+    static void setText(String player1_your_turn) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
